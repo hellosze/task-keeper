4c23366f767b2a43f5f7ef3ba7cbe3a1ef282e8e
Logan Yu <yu.logan@gmail.com>
2014-03-26 13:05:00 -0400

    added task body

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,3 @@
 class Task < ActiveRecord::Base
-  validates :title, :presence => true
+  validates :title, :body, :presence => true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140326024515) do
+ActiveRecord::Schema.define(version: 20140326165709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140326024515) do
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "body"
   end
 
 end
