bfb9fe347e493f9111019fc74db22e4f06d3926b
Logan Yu <yu.logan@gmail.com>
2014-03-26 15:24:28 -0400

    can click on entire task box

--- app/assets/javascripts/views/task_detail_view.js
+++ app/assets/javascripts/views/task_detail_view.js
@@ -27,18 +27,12 @@ TD.Views.TaskDetailView = Backbone.View.extend({
     var id = parseInt($currentTarget.attr("data-id"));
 
     task = that.model;
-    if (formData.task.title != "") {
-      task.set('title', formData.task.title);
-      task.set('body', formData.task.body);
-      task.save();
-    } else {
-      task.set('title', formData.task.title);
-      task.set('body', formData.task.body);
-      task.save();
-      $(event.currentTarget.parentElement).find('#task_title').attr("placeholder", "Title");
-    }
     
-    Backbone.history.navigate("#/id");
+    task.set('title', formData.task.title);
+    task.set('body', formData.task.body);
+    task.save();
+    
+    Backbone.history.navigate("#/tasks/" + id);
 
   },
   
