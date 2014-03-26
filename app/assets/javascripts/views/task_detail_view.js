32b7bcdc28b2f95ce4d29353fc8ddadcec4db23a
Logan Yu <yu.logan@gmail.com>
2014-03-26 12:11:48 -0400

    added save button to task detail

--- app/assets/javascripts/views/task_detail_view.js
+++ app/assets/javascripts/views/task_detail_view.js
@@ -1,4 +1,10 @@
 TD.Views.TaskDetailView = Backbone.View.extend({
+  
+  events: {
+    "click button.save": "save",
+    "click button.delete": "delete"
+  },
+  
   render: function () {
     var that = this;
 
@@ -8,5 +14,31 @@ TD.Views.TaskDetailView = Backbone.View.extend({
 
     that.$el.html(renderedContent);
     return that;
+  },
+  
+  save: function (event) {
+    event.preventDefault();
+    var that = this;
+    
+    var formData = $(event.currentTarget.parentElement).serializeJSON();
+    
+    var $currentTarget = $(event.currentTarget);
+    var id = parseInt($currentTarget.attr("data-id"));
+    
+    task = that.model;
+    if (formData.task.title != "") {
+      task.set('title', formData.task.title);
+      task.save();
+    } else {
+      formData.task.title = task.escape('title');
+      $(event.currentTarget.parentElement).find('input').val(task.escape('title'));
+    }
+    
+    Backbone.history.navigate("#/");
+
+  },
+  
+  delete: function () {
+    
   }
 });
